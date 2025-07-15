6f281093f827e0fcfc8840723f325b817fa9f48c
Ashay Rane <ashay@users.noreply.github.com>
2025-07-15 10:32:33 -0500

    [Wave] transfer lds work to main branch

--- iree/turbine/kernel/ops/wave_ops.py
+++ iree/turbine/kernel/ops/wave_ops.py
@@ -1169,6 +1169,7 @@ class Allocate(CustomOp):
     padding: int = 0
     parent: Optional[fx.Node] = None
     offset: Optional[IndexExpr] = None
+    hardware_transpose: LDSTransposeRead = None
 
     @property
     def indexing_dims(self) -> list[IndexSymbol]:
@@ -1552,6 +1553,7 @@ class Read(CustomOp):
     mapping_dynamic_vals: tuple[fx.Node, ...] = ()
     bounds: Optional[dict[IndexSymbol, IndexExpr]] = None
     _write_dependency: Optional[list[fx.Node]] = None
+    transpose = False
 
     @property
     def indexing_dims(self) -> list[IndexSymbol]:
--- iree/turbine/kernel/wave/codegen/handlers.py
+++ iree/turbine/kernel/wave/codegen/handlers.py
@@ -194,6 +194,7 @@ def handle_allocate(emitter: WaveEmitter, node: fx.Node):
             padding,
             parent,
             offset,
+            hardware_transpose
         ) = node.args
     except ValueError as e:
         raise ValidationError("Malformed arguments") from e
